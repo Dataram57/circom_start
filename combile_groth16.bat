64eabf4418583a11bed03760d606885aff719a0e
Dataram57 <dataram66657@gmail.com>
2025-05-21 12:07:24 +0200

    tools

---
+++ combile_groth16.bat
@@ -0,0 +1,3 @@
+circom circuit.circom --r1cs --sym --wasm
+snarkjs groth16 setup circuit.r1cs ceremony_final.ptau circuit_prover.zkey
+snarkjs zkey export verificationkey circuit_prover.zkey circuit_verifier.json
