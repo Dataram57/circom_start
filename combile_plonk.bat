bc741bdbcef4d912d4db296a8e8e5d65d2f158c2
Dataram57 <dataram66657@gmail.com>
2025-09-10 09:48:16 +0200

    tools

--- combile_plonk.bat
+++ combile_plonk.bat
@@ -1,3 +1,3 @@
-circom circuit.circom --r1cs --sym --wasm
-snarkjs plonk setup circuit.r1cs ceremony_final.ptau circuit_prover.zkey
-snarkjs zkey export verificationkey circuit_prover.zkey circuit_verifier.json
+call circom circuit.circom --r1cs --sym --wasm
+call snarkjs plonk setup circuit.r1cs ceremony_final.ptau circuit_prover.zkey
+call snarkjs zkey export verificationkey circuit_prover.zkey circuit_verifier.json
